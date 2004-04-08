c1caff0be7beb3a4468167e684dca519af636495
walco <none@none>
2004-04-08 15:57:38 +0000

    Added reply_spec to sysex.Command in order to parse type-less sysex replies. Unlike stated in the manual I start to think most replies are actually stateless.
    Added doc-tests, I really start to like them, especially now I write stuff to sys.stderr!
    I ran into a bug of the doc string compile stage which prevents the usage of '\x00', but
    this can be prevented by using ZERO (defined as '\x00').
    
    Improved parser to the extent the first command get_disklist() now returns meaningful python output!
    
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40454

--- src/disktools.py
+++ src/disktools.py
@@ -9,116 +9,177 @@ __version__=  '0.1'
 
 import sysex
 def update_disklist(z48):
-     """ Update the list of disks connected
+     """Update the list of disks connected
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x01',update_disklist,(None,None),())
+     comm =  sysex.Command('\x20','\x01', (None,None), (), ())
      return z48.execute(comm)
 
 def select_disk(z48, arg):
-     """ Select Disk <Data1> = Disk Handle
+     """Select Disk <Data1> = Disk Handle
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x02',select_disk,(sysex.WORD,None),(arg))
+     comm =  sysex.Command('\x20','\x02', (sysex.WORD,None), (arg), ())
      return z48.execute(comm)
 
 def test_disk(z48, arg):
-     """ Test if the disk is valid <Data1> = Disk Handle
+     """Test if the disk is valid <Data1> = Disk Handle
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x03',test_disk,(sysex.WORD,None),(arg))
+     comm =  sysex.Command('\x20','\x03', (sysex.WORD,None), (arg), ())
      return z48.execute(comm)
 
 def get_no_disks(z48):
-     """ Get the number of disks connected
+     """Get the number of disks connected
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x04',get_no_disks,(None,None),())
+     comm =  sysex.Command('\x20','\x04', (None,None), (), ())
      return z48.execute(comm)
 
 def get_disklist(z48):
-     """ Get list of all connected disks
+     """Get list of all connected disks
+
+     Returns:
+          sysex.WORD
+          sysex.BYTE
+          sysex.BYTE
+          sysex.WORD
+          sysex.STRING     
      """
-     comm =  sysex.Command('\x20','\x05',get_disklist,(None,None),())
+     comm =  sysex.Command('\x20','\x05', (None,None), (), (sysex.WORD, sysex.BYTE, sysex.BYTE, sysex.WORD, sysex.STRING))
      return z48.execute(comm)
 
 def get_curr_path(z48):
-     """ Get current path of current disk
+     """Get current path of current disk
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x09',get_curr_path,(None,None),())
+     comm =  sysex.Command('\x20','\x09', (None,None), (), ())
      return z48.execute(comm)
 
 def eject_disk(z48, arg):
-     """ Eject Disk <Data1> = Disk Handle
+     """Eject Disk <Data1> = Disk Handle
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x0D',eject_disk,(sysex.WORD,None),(arg))
+     comm =  sysex.Command('\x20','\x0D', (sysex.WORD,None), (arg), ())
      return z48.execute(comm)
 
 def get_no_subfolders(z48):
-     """ Get number of sub-folders in the current folder.
+     """Get number of sub-folders in the current folder.
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x10',get_no_subfolders,(None,None),())
+     comm =  sysex.Command('\x20','\x10', (None,None), (), ())
      return z48.execute(comm)
 
 def get_subfolder_names(z48):
-     """ Get the names of all of the sub-folders in the current folder.
+     """Get the names of all of the sub-folders in the current folder.
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x12',get_subfolder_names,(None,None),())
+     comm =  sysex.Command('\x20','\x12', (None,None), (), ())
      return z48.execute(comm)
 
 def set_curr_folder(z48, arg):
-     """ Open Folder. This sets the current folder to be the requested one. (If <Data1> = 0, the root folder will be selected.)
+     """Open Folder. This sets the current folder to be the requested one. (If <Data1> = 0, the root folder will be selected.)
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x13',set_curr_folder,(sysex.STRING,None),(arg))
+     comm =  sysex.Command('\x20','\x13', (sysex.STRING,None), (arg), ())
      return z48.execute(comm)
 
 def load_folder(z48, arg):
-     """ Load Folder: the selected folder, and all its contents (including sub- 
+     """Load Folder: the selected folder, and all its contents (including sub- 
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x15',load_folder,(sysex.STRING,None),(arg))
+     comm =  sysex.Command('\x20','\x15', (sysex.STRING,None), (arg), ())
      return z48.execute(comm)
 
 def create_subfolder(z48, arg):
-     """ Create Folder: Creates a sub-folder in the currently selected folder.
+     """Create Folder: Creates a sub-folder in the currently selected folder.
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x16',create_subfolder,(sysex.STRING,None),(arg))
+     comm =  sysex.Command('\x20','\x16', (sysex.STRING,None), (arg), ())
      return z48.execute(comm)
 
 def del_subfolder(z48, arg):
-     """ Delete Sub-Folder.
+     """Delete Sub-Folder.
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x17',del_subfolder,(sysex.STRING,None),(arg))
+     comm =  sysex.Command('\x20','\x17', (sysex.STRING,None), (arg), ())
      return z48.execute(comm)
 
 def rename_subfolder(z48, arg1, arg2):
-     """ Rename Folder: <Data1> = name of folder to rename
+     """Rename Folder: <Data1> = name of folder to rename
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x18',rename_subfolder,(sysex.STRING,sysex.STRING),(arg1, arg2))
+     comm =  sysex.Command('\x20','\x18', (sysex.STRING,sysex.STRING), (arg1, arg2), ())
      return z48.execute(comm)
 
 def get_no_files(z48):
-     """ Get number of files in the current folder.
+     """Get number of files in the current folder.
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x20',get_no_files,(None,None),())
+     comm =  sysex.Command('\x20','\x20', (None,None), (), ())
      return z48.execute(comm)
 
 def get_filenames(z48):
-     """ Get the names of all of the files in the current folder.
+     """Get the names of all of the files in the current folder.
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x22',get_filenames,(None,None),())
+     comm =  sysex.Command('\x20','\x22', (None,None), (), ())
      return z48.execute(comm)
 
 def rename_file(z48, arg1, arg2):
-     """  Rename File 
+     """ Rename File 
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x28',rename_file,(sysex.STRING,sysex.STRING),(arg1, arg2))
+     comm =  sysex.Command('\x20','\x28', (sysex.STRING,sysex.STRING), (arg1, arg2), ())
      return z48.execute(comm)
 
 def delete_file(z48, arg):
-     """ Delete File. <Data1> = name of file to delete.
+     """Delete File. <Data1> = name of file to delete.
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x29',delete_file,(sysex.STRING,None),(arg))
+     comm =  sysex.Command('\x20','\x29', (sysex.STRING,None), (arg), ())
      return z48.execute(comm)
 
 def load_file(z48, arg):
-     """ Load File <Data1> = name of file to load.
+     """Load File <Data1> = name of file to load.
+
+     Returns:
+               
      """
-     comm =  sysex.Command('\x20','\x2A',load_file,(sysex.STRING,None),(arg))
+     comm =  sysex.Command('\x20','\x2A', (sysex.STRING,None), (arg), ())
      return z48.execute(comm)
 
--- src/sysex.py
+++ src/sysex.py
@@ -7,11 +7,11 @@ class Command:
 
     """
 
-    def __init__(self, section_id, id, name, arg_types,args):
+    def __init__(self, section_id, id, arg_types,args, reply_spec=None):
         self.section_id = section_id
         self.id = id
-        self.name = name
         self.arg_types = ()
+        self.reply_spec = reply_spec
 
         for arg_type in arg_types:
             if arg_type is not None:
@@ -56,9 +56,21 @@ class Reply:
     >>> reply = Reply(''.join(bytes)) 
     >>> reply.parse()
     1
+
+    >>> bytes =  (START_SYSEX, AKAI_ID, Z48_ID, USERREF, REPLY_ID_REPLY, '\x20','\x05', '\x01', END_SYSEX)
+    >>> reply = Reply(''.join(bytes), (BYTE)) 
+    >>> reply.parse()
+    1
+
+    >>> bytes =  (START_SYSEX, AKAI_ID, Z48_ID, USERREF, REPLY_ID_REPLY,\
+    '\x20\x05', ZERO,'\x02\x01\x02', ZERO, '\x01\x5a\x34\x38\x20\x26\x20\x4d\x50\x43\x34\x4b', ZERO, END_SYSEX)
+    >>> reply = Reply(''.join(bytes), (WORD, BYTE, BYTE, WORD, STRING)) 
+    >>> reply.parse() 
+    [512, 1, 2, 256, 'Z48 & MPC4K']
     """
-    def __init__(self, bytes):
+    def __init__(self, bytes, reply_spec=()):
         self.bytes = bytes
+        self.reply_spec = reply_spec
 
     def parse(self):
         """ Parses the command sequence
@@ -67,8 +79,14 @@ class Reply:
         if self.bytes[0] != START_SYSEX or self.bytes[-1] != END_SYSEX:
             raise ParseException("Invalid system exclusive string received")
 
-        result = []
-        i = 4 # skip start, vendor id, z8 id, userref (TODO:consider the user ref)
+        # ugly hack to work around malformatted replies TODO: figure out what the real problem is!
+        start_index = self.bytes.find(START_SYSEX, 1)
+        if  start_index > 0:
+            i = start_index 
+        else:
+            i = 0
+
+        i += 4 # skip start, vendor id, z8 id, userref (TODO:consider the user ref)
         reply_id = self.bytes[i]
         if reply_id == REPLY_ID_OK:
             return []
@@ -82,11 +100,32 @@ class Reply:
         else:
             raise ParseException("Unknown reply type: %02x" % struct.unpack('b', reply_id))
 
-        i +=  3 # skip the section and command (TODO: reconsider)
-        while i < (len(self.bytes) - 1):
-            part, len_parsed = parse_byte_string(self.bytes, i)
+        i +=  3 # skip past the reply, section and command (TODO: reconsider)
+
+        if len(self.reply_spec) > 0:
+            return self.parse_untyped_bytes(self.bytes, i)
+        else:
+            return self.parse_typed_bytes(self.bytes, i)
+
+    def parse_typed_bytes(self, bytes, offset):
+        result = []
+        while offset < (len(bytes) - 1):
+            part, len_parsed = parse_byte_string(bytes, offset)
             result.append(part)
-            i += len_parsed
+            offset += len_parsed
+        if len(result) == 1:
+            return result[0]
+        else:
+            return result
+
+    def parse_untyped_bytes(self, bytes, offset):
+        """Parses a byte string without type information
+        """
+        result = []
+        for type in self.reply_spec:
+            part, len_parsed = parse_byte_string(bytes, offset, type) 
+            result.append(part)
+            offset += len_parsed
         if len(result) == 1:
             return result[0]
         else:
@@ -104,7 +143,7 @@ class Error(Exception):
 
 # Module vars and methods
 
-INT         = '\x00'
+ZERO        = '\x00'
 BYTE        = '\x01'
 SBYTE       = '\x02'
 WORD        = '\x03' # 2 BYTES
@@ -117,6 +156,10 @@ STRING      = '\x09'
 TWO_BYTES   = '\x10'
 THREE_BYTES = '\x11'
 CUSTOM      = '\x20'
+
+# arrays TODO: think of something smarter!
+BYTE01      = '\x10'
+BYTE02      = '\x11'
 
 POSTIVE     = '\x00'
 NEGATIVE    = '\x01'
@@ -138,8 +181,8 @@ END_SYSEX = '\xf7'
 def _to_byte_string(src_type, value):
     """
     """
-    # TODO: raise valueerrors if value out of range 
-    if (src_type == BYTE or src_type == INT):
+    # TODO: raise value errors if value out of range 
+    if (src_type == BYTE or src_type == ZERO):
        byte_string = struct.pack('<b', value)
     elif (src_type == SBYTE):
        byte_string = struct.pack('<b', value)
@@ -161,96 +204,107 @@ def _to_byte_string(src_type, value):
     
     return byte_string
 
-def parse_byte_string(data, offset=0):
-    """ Parses a bytestring
+def parse_byte_string(data, offset=0, type=None):
+    """ Parses a byte string
 
-    >>> parse_byte_string((STRING, '\x54','\x45','\x53','\x54', STRING_TERMINATOR))
+    >>> parse_byte_string(STRING + '\x54\x45\x53\x54' + STRING_TERMINATOR)
     ('TEST', 6)
 
-    >>> parse_byte_string((BYTE + '\x0f'))
+    >>> parse_byte_string('\x54\x45\x53\x54' + STRING_TERMINATOR, 0, STRING)
+    ('TEST', 5)
+
+    Type info in the data: 
+    >>> parse_byte_string(BYTE + '\x0f')
     (15, 2)
 
+    No type info in the data: 
+    >>> parse_byte_string('\x0f', 0, BYTE)
+    (15, 1)
 
-    >>> parse_byte_string((SBYTE + '\x01\x0f'))
+    >>> parse_byte_string(SBYTE + '\x01\x0f')
     (-15, 3)
 
-    >>> parse_byte_string((WORD + '\x0f\x0f'))
+    >>> parse_byte_string(WORD + '\x0f\x0f')
     (3855, 3)
 
-    >>> parse_byte_string((SWORD + '\x01\x0f\x0f'))
+    >>> parse_byte_string(SWORD + '\x01\x0f\x0f')
     (-3855, 4)
 
-    >>> parse_byte_string((DWORD + '\x00\x0f\x0f'))
-    (3855, 4)
+    >>> parse_byte_string(DWORD + '\x01\x01\x01\x0f')
+    (251724033L, 5)
 
-    >>> parse_byte_string((SDWORD + '\x01\x00\x0f\x0f'))
-    (3855, 5)
+    >>> parse_byte_string(SDWORD + '\x01\x01\x01\x0f\x0f')
+    (-252641537L, 6)
     """
 
-    dest_type = data[offset]
-    len_parsed_data = 1 
-    if (dest_type == BYTE or dest_type == INT):
-        result = struct.unpack('b', data[offset+1])[0]
+    len_parsed_data = 0
+
+    if type == None:
+        type = data[offset]
+        offset += 1
+        len_parsed_data += 1 
+        
+    if (type == BYTE or type == ZERO):
+        result = struct.unpack('b', data[offset])[0]
         len_parsed_data += 1
 
-    elif (dest_type == SBYTE):
-        result = struct.unpack('b', data[offset+2])[0]
+    elif (type == SBYTE):
+        result = struct.unpack('b', data[offset+1])[0]
 
-        if data[offset+1] == NEGATIVE:
+        if data[offset] == NEGATIVE:
             result *= -1 
          
         len_parsed_data += 2;
 
-    elif (dest_type == WORD):
-        result = struct.unpack('<H', data[offset+1:offset+3])[0]
+    elif (type == WORD or type == TWO_BYTES):
+        result = struct.unpack('<H', data[offset:offset+2])[0]
         len_parsed_data += 2;
 
-    elif (dest_type == SWORD):
-        result = struct.unpack('<H', data[offset+2:offset+4])[0]
+    elif (type == SWORD):
+        result = struct.unpack('<H', data[offset+1:offset+3])[0]
+
+        if data[offset] == NEGATIVE:
+            result *= -1 
+
+        len_parsed_data += 3;
+    elif (type == DWORD):
+        result = struct.unpack('<I', data[offset:offset+4])[0]
+        len_parsed_data += 4;
+
+    elif (type == SDWORD):
+        result = struct.unpack('<I', data[offset+1:offset+5])[0]
 
         if data[offset+1] == NEGATIVE:
             result *= -1 
 
-        len_parsed_data += 3;
-    elif (dest_type == DWORD):
-        result = struct.unpack('<I', data[offset+1:offset+5])[0]
-
-        len_parsed_data += 3;
-    elif (dest_type == SDWORD):
-        result = struct.unpack('<I', data[offset+1:offset+5])[0]
         len_parsed_data += 5;
-
-        result = struct.unpack('<I', data[offset+2:offset+4])[0]
-    elif (dest_type == STRING):
-        bytes = []
-        for char in data[1:]:
-            len_parsed_data += 1;
+    elif (type == STRING):
+        index = 0
+        for char in data[offset:]:
             if char == STRING_TERMINATOR:
-                break
-            else:
-                bytes.append(_convert_akai_char(char))
-        result = ''.join(bytes)
+                result = struct.unpack( str(index) + 's', data[offset:offset+index])[0]
+            index += 1
+
+        len_parsed_data += index
     else: 
-        raise ValueError("unsupported type %s" % repr(dest_type))
+        raise ValueError("unsupported type %s" % repr(type))
         
     return (result, len_parsed_data)
+
+def _to_string(ordvalues):
+    """Method to quickly scan a string 
+    >>> ordvalues = (90, 52, 56, 32, 38, 32, 77, 80, 67, 52, 75)
+    >>> _to_string(ordvalues)
+    'Z48 & MPC4K'
+    """
+    return ''.join([chr(value) for value in ordvalues])
+
 
 def _to_akai_char(value):
     """ Converts a python char to an string encoded ordinal value
         TODO: Support for cursor values
     """
     return hex(ord(value))[2:]
-
-def _convert_akai_char(value):
-    """ Converts an akai string encoded ascii value to a python string
-
-    >>> _convert_akai_char('\x3f')
-    '?'
-    """
-    value = struct.unpack('b', value)[0]
-    assert value > 0 and value < 256
-    return str(chr(value))
-
 
 if __name__ == "__main__":
     import doctest, sys
