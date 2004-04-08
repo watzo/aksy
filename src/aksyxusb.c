2129a9b9a40ffc558f17f9ce681ca5f9a6b18230
walco <none@none>
2004-04-08 15:42:31 +0000

    Cleaned up the code, added reset_usb and reset_usb_conn methods (They don't seem to be very effective yet...)
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40453

--- src/aksyxusb.c
+++ src/aksyxusb.c
@@ -47,28 +47,28 @@ Z48Sampler_init_usb(PyObject *self, PyObject *args)
 	}
 	else
 	{
-		// int retval = usb_set_configuration(akai_z48, 1);
-		int retval = usb_claim_interface(akai_z48, 0);
-		printf("Retval: %i \n", retval);
-		// retval = usb_set_altinterface(akai_z48, 0);
+		int rc = usb_claim_interface(akai_z48, 0);
+		printf("Retval: %i \n", rc);
 
-		if (retval)
+		if (rc)
 		{
 			return PyErr_Format(PyExc_Exception, "USB initialization failed. Check permissions on device.");
 		}
 
 
+		/* setup sequence (sniffed from windows usblog) */
 		char setupmsg[] = "\x03\x01";
-	    //char msg1[] = "\x10\x08\x0\xF0\x47\x5F\x0\x20\x5\xF7";
+	    rc = usb_bulk_write(akai_z48, EP_OUT, setupmsg, 2, 5000);
+
+		/* turn of confirmation messages */
 	    char msg[] = "\x10\x08\x00\xF0\x47\x5F\x00\x00\x01\x00\xF7";
-	    retval = usb_bulk_write(akai_z48, 0x82, setupmsg, 2, 5000);
 		unsigned char buffer[8];
-	    retval = usb_bulk_write(akai_z48, 0x82, msg, 11, 5000);
-	 	retval = usb_bulk_read(akai_z48,0x02,buffer,8,1000);
+	    rc = usb_bulk_write(akai_z48, EP_OUT, msg, 11, 5000);
+	 	rc = usb_bulk_read(akai_z48,EP_IN,buffer,8,1000);
 
 		/*
 		int i;
-		for(i=0; i<retval; i++) {
+		for(i=0; i<rc; i++) {
        		printf("%02x", buffer[i]);
         	if(i % 16 == 15) printf("\n");
         	else printf(" ");
@@ -90,14 +90,48 @@ Z48Sampler_close_usb(PyObject *self, PyObject *args)
 		return PyErr_Format(PyExc_Exception, "USB was not initialized so couldn't not closed");
 	}
 
-	int retval = usb_release_interface(akai_z48, 0);
-	retval = usb_close (akai_z48)|retval;
+	int rc = usb_release_interface(akai_z48, 0);
+	rc = usb_close (akai_z48)|rc;
 	akai_z48 = 0;
 
-	if (retval)
+	if (rc)
 	{
-		
 		return PyErr_Format(PyExc_Exception, "USB was not successfully closed");
+	}
+
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject* 
+Z48Sampler_reset_usb(PyObject *self, PyObject *args)
+{
+	if (!akai_z48)
+	{
+		return PyErr_Format(PyExc_Exception, "USB was not initialized so couldn't not reset");
+	}
+
+	int rc = usb_reset(akai_z48);
+
+	if (rc)
+	{
+		return PyErr_Format(PyExc_Exception, "USB was not successfully reset");
+	}
+	
+	akai_z48 = 0;
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject* 
+Z48Sampler_reset_usb_conn(PyObject *self, PyObject *args)
+{
+	int rc = usb_clear_halt(akai_z48, EP_IN);
+	rc = rc | usb_clear_halt(akai_z48, EP_OUT);
+
+	if (rc)
+	{
+		return PyErr_Format(PyExc_Exception, "USB connection was not successfully reset.");
 	}
 
     Py_INCREF(Py_None);
@@ -122,12 +156,12 @@ Z48Sampler_execute(PyObject* self, PyObject* args)
 	}
 	else
 	{
-		int retval = usb_bulk_write(akai_z48, 0x82, sysex_command, string_length, 1000);
+		usb_bulk_write(akai_z48, EP_OUT, sysex_command, string_length, 1000);
 		unsigned char* buffer = (unsigned char*)PyMem_Malloc( 64 * sizeof(unsigned char));
-		retval = usb_bulk_read(akai_z48,0x02,buffer,64,1000);  
-   		printf("Z48Sampler.execute finished with retval %i\n", retval);
+		int rc = usb_bulk_read(akai_z48,EP_IN,buffer,64,1000);  
+   		printf("Z48Sampler.execute finished with rc %i\n", rc);
 
-		if (retval < 0)
+		if (rc < 0)
 		{
 			PyMem_Free(buffer);
 			return PyErr_Format(PyExc_Exception, "Error reading sysex reply.");
@@ -136,14 +170,15 @@ Z48Sampler_execute(PyObject* self, PyObject* args)
 		PyObject* ret;
 
 		/* debug printing */
-		int i = 0;
-		for(i=0; i<retval; i++) 
+		int i;
+		for(i = 0; i < rc; i++) 
 		{
 			printf("%02x", buffer[i]);
-			if(i % 16 == 15) printf("\n");
+			if(i % 16 == 15 || i == rc - 1) printf("\n");
 			else printf(" ");
 		}
-		ret = Py_BuildValue("s#", buffer, retval); 
+
+		ret = Py_BuildValue("s#", buffer, rc); 
 		PyMem_Free(buffer);
 		return ret;
 	}
@@ -152,8 +187,10 @@ Z48Sampler_execute(PyObject* self, PyObject* args)
 static PyMethodDef Z48SamplerMethods[] = 
 {
     {"__init__", Z48Sampler_init, METH_O, "init"},
-    {"init_usb", Z48Sampler_init_usb, METH_O, "Initializes USB device and end point"},
-    {"close_usb", Z48Sampler_close_usb, METH_O, "Closes USB device and end point"},
+    {"init_usb", Z48Sampler_init_usb, METH_O, "Initializes USB device and interface."},
+    {"_reset_usb", Z48Sampler_reset_usb, METH_O, "Resets USB device."},
+    {"reset_usb_conn", Z48Sampler_reset_usb_conn, METH_O, "Resets USB endpoints."},
+    {"close_usb", Z48Sampler_close_usb, METH_O, "Closes USB device and interface."},
     {"_execute", Z48Sampler_execute, METH_VARARGS, "Executes a Sysex string on the sampler"},
     {NULL},
 };
@@ -169,7 +206,7 @@ void initaksyxusb()
     PyMethodDef *def;
 
     /* create a new module and class */
-	/* TODO: extend object */
+	/* TODO: prevent multiple instances */
     PyObject *module = Py_InitModule("aksyxusb", ModuleMethods);
     PyObject *moduleDict = PyModule_GetDict(module);
     PyObject *classDict = PyDict_New();
--- src/aksyxusb.h
+++ src/aksyxusb.h
@@ -1,3 +1,5 @@
 #define VENDOR_ID 0x9E8 
 #define PRODUCT_ID 0x05F
+#define EP_OUT 0x82
+#define EP_IN 0x02
 extern void initaksyxusb(void);
