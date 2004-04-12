6dfd010f0c555baff42fd98bfa5f3f3bc86e305d
walco <none@none>
2004-04-12 20:36:09 +0000

    Added register method for commands, removed the __new__ (have to get a working singleton impl for extension classes). Fixed the sysex bytestring sent to the sampler, which didn't encode string length properly
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40457

--- src/aksy.py
+++ src/aksy.py
@@ -12,40 +12,48 @@ class Z48(Z48Sampler):
     >>> z.init_usb()
     >>> z.get_no_disks()
     1
-    >>> z.get_disklist() 
-    [512, 1, 2, 256, 'Z48 & MPC4K']
+    >>> # z.get_disklist() 
+    # [512, 1, 2, 256, 'Z48 & MPC4K']
+
+    # it now says done!
+    >>> z.select_disk(512) 
+    >>> z.set_curr_folder('') 
+    >>> z._execute('\x10\x0a\x00\xf0\x47\x5e\x20\x00\x00\x10\x20\x30\xf7')
+    >>> z.get_no_subfolders() 
+    >>> z.get_subfolder_names() 
     >>> z.close_usb()
     """
 
     def __init__(self):
         Z48Sampler.__init__(self)
         Z48.__dict__.update(disktools.__dict__)
-
-    """
-    def __new__(cls):
-        print cls.__name__
-        if not Z48.__dict__.has_key(cls.__name__):
-           Z48.__dict[cls.__name__] =  Z48Sampler.__new__(cls)
-
-        return Z48.__dict__.get(cls.__name__) 
-
-    """
+        self.commands = {}
+        self.register_disktools()
 
     def init(self):
         """Initializes the connection with the sampler
         """
         Z48Sampler.init_usb(self)
 
+    def reset(self):
+        """Initializes the connection with the sampler
+        """
+        result_bytes = self._execute('\x30')
+        print 'bytes: %s' % result_bytes
+
     def close(self):
         """Closes the connection with the sampler
         """
         Z48Sampler.close_usb(self)
 
-    def execute(self, command):
-        request = Request(command)
-        result_bytes = self._execute('\x10\x08\x00' + request.get_bytes())
-        sys.stderr.writelines("Python: len: %i data: %s\n" %(len(result_bytes), repr(struct.unpack(str(len(result_bytes)) + 'b', result_bytes))))
+    def execute(self, command, args):
+        request = Request(command, args)
+        sys.stderr.writelines("Request: %s\n" % repr(request))
+        
+        result_bytes = self._execute('\x10' + struct.pack('b', len(request.get_bytes())) + '\x00' + request.get_bytes())
+        sys.stderr.writelines("Length of reply: %i\n" % len(result_bytes))
         result = Reply(result_bytes, command.reply_spec)
+        sys.stderr.writelines("Reply: %s\n" % repr(result))
         return result.parse()
 
 if __name__ == "__main__":
