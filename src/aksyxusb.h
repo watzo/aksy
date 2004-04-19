d0886a8d57403d982716006120fcd59194711502
walco <none@none>
2004-04-19 22:19:04 +0000

    Upload fixes
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40473

--- src/aksyxusb.h
+++ src/aksyxusb.h
@@ -4,7 +4,8 @@
 #define EP_IN 0x02
 #define USB_TIMEOUT 1000
 #define Z48_GET 0x41
-#define Z48_PUT 0x20
+#define Z48_MEMORY_PUT 0x20
+#define Z48_DISK_PUT 0x00
 #define GET_BLOCK_SIZE(buffer) (buffer[7] | buffer[6] << 8 | buffer[5] << 16 | buffer[4] << 24)
 #define GET_BYTES_TRANSFERRED(buffer) (buffer[3] | buffer[2] << 8 | buffer[1] << 16 | buffer[0] << 24)
 extern void initaksyxusb(void);
