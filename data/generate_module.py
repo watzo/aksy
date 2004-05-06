6e0d86032ca6f91471289b2c77407c9638972d9b
walco <none@none>
2004-05-06 19:57:06 +0000

    Fixed method generation, added classname is now CamelCase
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40485

--- data/generate_module.py
+++ data/generate_module.py
@@ -15,6 +15,12 @@ def method_name_helper(desc):
     desc = desc.split(' ')
     return '_'.join([word.lower() for word in desc[0:3]])
 
+def classname_helper(section_name):
+    """ Creates classname based on the section name
+    """
+    names = section_name.split('_')
+    return ''.join([name.capitalize() for name in names])
+
 # could use ljust etc...
 indent_block = "     "
 sysex_module_name = 'sysex'
@@ -34,13 +40,12 @@ line = file_in.readline()
 # Section data
 section_id, section_name, section_desc = line[:-1].split('\t')
 destfile = section_name + '.py'
-classname = section_name
 
 file_out = open(destfile, 'w')
 file_out.writelines( "\n\"\"\" Python equivalent of akai section %s\n\n%s\n\"\"\"\n\n" % (section_name,section_desc)) 
 file_out.writelines( "%s\n" % preamble) 
 file_out.writelines( "import %s\n\n" % sysex_module_name ) 
-file_out.writelines( "def %s:\n" % classname)
+file_out.writelines( "def %s:\n" % classname_helper(section_name))
 
 methods = StringIO.StringIO()
 
@@ -56,7 +61,7 @@ while line:
         name = elems[1]
         desc = elems[2]
 
-        args = []
+        args = ['self']
         data = []
         for i in range(3, len(elems)-1):
             if elems[i] != 'NA':
@@ -88,10 +93,10 @@ while line:
         comm_args.extend(args[1:])
         comm_args.append('')
         methods.writelines( 
-            "%scomm =  %s.commands.get(('%s','%s'))\n" \
-            % (indent_block*2, z48_instance_name, section_id, id))
+            "%scomm =  self.commands.get(('%s','%s'))\n" \
+            % (indent_block*2, section_id, id))
 
-        methods.writelines( "%sreturn %s.execute(comm, %s)\n\n" % (indent_block*2, z48_instance_name, '('+ ', '.join(comm_args) + ')'))
+        methods.writelines( "%sreturn self.%s.execute(comm, %s)\n\n" % (indent_block*2, z48_instance_name, '('+ ', '.join(comm_args) + ')'))
 
         # put the command in a dict with tuple key (section_id, id) 
         file_out.writelines( 
