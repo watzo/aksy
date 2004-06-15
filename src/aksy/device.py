417220da37cdb541b6fdc712b6a3a08e6ae047f2
walco <none@none>
2004-06-15 19:08:33 +0000

    Renamed and remodelled sampler to device
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40559

---
+++ src/aksy/device.py
@@ -0,0 +1,78 @@
+import sys, aksy
+from aksy.devices import config
+import imp
+
+class Devices:
+    """
+    >>> Devices.get_instance('z48', 'usb')
+    1
+    >>> Devices.get_instance('mock_z48', None)
+    """
+
+    _devices = config.devices 
+    def get_instance(name, type=None, *args, **kwargs):
+        module_name, klass = Devices._devices[(name, type)]
+        sys.stderr.writelines(module_name)
+        mod = __import__(module_name)
+        components = module_name.split('.')
+        for comp in components[1:]:
+            mod = getattr(mod, comp)
+        sys.stderr.writelines(repr(mod))
+        if not hasattr(mod, klass):
+            raise Exception('Device %s not found' %name)
+        return getattr(mod, klass)(*args, **kwargs)
+    
+    get_instance = staticmethod(get_instance)
+
+class Device:
+    """Defines a generic device.
+
+    """
+
+    def discover(self):
+        """
+        """
+
+    def get_vendor_id(self):
+        raise NotImplementedError
+
+    def get_system_objects(self):
+        """Returns the system objects contained by this device
+        """
+        raise NotImplementedError
+
+    def init(self):
+        """Initializes the device
+        """
+        raise NotImplementedError
+
+    def close(self):
+        """Closes the device
+        """
+        raise NotImplementedError
+
+    def execute(self, command, args, device_id=None, extra_id=None):
+        """Execute a command on the device
+        """
+        raise NotImplementedError
+
+class Command:
+    """Defines a command which can be executed on the device
+    """
+
+class Request:
+    """Maps a command to a command sequence
+    """
+
+class Reply:
+    """Maps the command reply sequence to aksy types
+    """
+
+
+#Device.register('z48', 'aksy.z48.device' ,'Sampler')
+#Device.register('mock_z48', 'aksy.mock_z48.sampler','Sampler')
+
+
+if __name__ == "__main__":
+    import doctest, sys
+    doctest.testmod(sys.modules[__name__])
