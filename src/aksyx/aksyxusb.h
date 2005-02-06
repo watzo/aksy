60fc9745b9eec324b17f3215650939058e2d1729
walco <none@none>
2005-02-06 21:57:47 +0000

    Diskput command added
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40597

--- src/aksyx/aksyxusb.h
+++ src/aksyx/aksyxusb.h
@@ -7,7 +7,7 @@
 #define Z48_DISK_GET 0x41
 #define Z48_MEMORY_GET 0x22
 #define Z48_MEMORY_PUT 0x20
-#define Z48_DISK_PUT 0x00
+#define Z48_DISK_PUT 0x40
 #define GET_BLOCK_SIZE(buffer) (buffer[7] | buffer[6] << 8 | buffer[5] << 16 | buffer[4] << 24)
 #define GET_BYTES_TRANSFERRED(buffer) (buffer[3] | buffer[2] << 8 | buffer[1] << 16 | buffer[0] << 24)
 extern void initaksyxusb(void);
