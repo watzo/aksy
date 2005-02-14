1aacce32c28e57f8e97bca6450753af5244091ab
walco <none@none>
2005-02-14 21:52:47 +0000

    Reflect changes in dir structure
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40606

---
+++ src/aksy/devices/config.py
@@ -0,0 +1,4 @@
+devices = { ('z48', 'usb'): ('aksy.devices.akai.z48.sampler', 'Sampler'),
+            ('mock_z48', None): ('aksy.devices.mock_z48.sampler', 'Sampler'),
+            ('gx700','midi'): ('aksy.devices.gx700.devices' ,'RackEffect'),
+          }
