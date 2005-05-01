064b107b236ade2bce4b5e93cf3f5f2fda1f02d2
walco <none@none>
2005-05-01 15:55:02 +0000

    Minor tweaks.
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40628

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-
-from distutils.core import setup,Extension
-
-setup(name = "aksy usb extension",
+"""Aksy setup module
+"""
+from distutils.core import setup, Extension
+from distutils.command import bdist_wininst
+setup(name = "aksyx usb extension",
       version = "0.01",
       author = "Walco van Loon",
       author_email = "walco at n--tree.net",
-      url='http://walco.n--tree.net/aksy',
+      url='http://walco.n--tree.net/projects/aksy',
 	  ext_modules=[
          Extension("aksyxusb",
                 sources = [ "src/aksyx/aksyxusb.c", "src/aksyx/akaiusb.c" ],
