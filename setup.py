caefba2dbe5c594a5c20f4f01d7725faa79cab1c
walco <none@none>
2005-05-18 21:39:04 +0000

    * Added build options for MacOSX.
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40686

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 """Aksy setup module
 """
 from distutils.core import setup, Extension
+import platform
+
+extra_link_args = []
+if platform.system() == "Darwin":
+    extra_link_args = ['-framework CoreFoundation IOKit']
+
+print os.name
 setup(name = "aksy",
-      version = "0.1",
+      version = "0.1.1",
       author = "Walco van Loon",
       author_email = "walco at n--tree.net",
       url='http://walco.n--tree.net/projects/aksy',
@@ -10,10 +17,9 @@ setup(name = "aksy",
       ext_modules=[
           Extension("aksyxusb",
               sources = [ "src/aksyx/aksyxusb.c", "src/aksyx/akaiusb.c" ],
-              define_macros=[
-                  ('_DEBUG', '1'),
-                  ('_BIG_ENDIAN', 0),
-              ],
-              libraries = [ "usb" ],
+              define_macros=[('_DEBUG', '1')],
+              extra_link_args = extra_link_args,
+              libraries = ["usb"],
           ),
-       ])
+      ]
+)
