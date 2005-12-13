7897b4f6400753a3e500c8a0e4a9ea549fa0a9f4
walco <none@none>
2005-12-13 21:20:02 +0000

    Updated device config to new class hierarchy
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40784

--- src/aksy/devices/config.py
+++ src/aksy/devices/config.py
@@ -1,4 +1,5 @@
-devices = { ('akai', 'usb'): ('aksy.devices.akai.sampler', 'Sampler'),
+devices = { ('z48', 'usb'): ('aksy.devices.akai.z48.sampler', 'Z48'),
+            ('mpc4k', 'usb'): ('aksy.devices.akai.z48.sampler', 'MPC4K'),
+            ('s56k', 'usb'): ('aksy.devices.akai.s56k.sampler', 'S56K'),
             ('mock_z48', None): ('aksy.devices.akai.mock_z48.sampler', 'Sampler'),
-            ('gx700','midi'): ('aksy.devices.gx700.devices' ,'RackEffect'),
           }
