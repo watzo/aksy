8816f9addabae411f2c7dddb2ee4f82296d282c6
walco <none@none>
2005-12-24 20:23:31 +0000

    Command fixes
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40821

--- src/aksy/devices/akai/z48/disktools.py
+++ src/aksy/devices/akai/z48/disktools.py
@@ -1,5 +1,5 @@
 
-""" Python equivalent of akai section disktools
+""" Disktools
 
 Methods to manipulate the samplers filesystem
 """
@@ -14,32 +14,38 @@ class Disktools:
           self.z48 = z48
           self.update_disklist_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x01', 'update_disklist', (), None,
               userref_type=aksy.devices.akai.sysex_types.S56K_USERREF)
-          self.select_disk_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x02', 'select_disk', (aksy.devices.akai.sysex_types.WORD,), None)
+          self.select_disk_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x02', 'select_disk', (aksy.devices.akai.sysex_types.WORD,), None,
+              userref_type=aksy.devices.akai.sysex_types.S56K_USERREF)
           self.test_disk_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x03', 'test_disk', (aksy.devices.akai.sysex_types.WORD,), None)
           self.get_no_disks_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x04', 'get_no_disks', (), None)
           self.get_disklist_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x05', 'get_disklist', (),
               (aksy.devices.akai.sysex_types.DISKLIST,), userref_type=aksy.devices.akai.sysex_types.S56K_USERREF)
           self.get_curr_path_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x09', 'get_curr_path', (), None)
           self.eject_disk_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x0D', 'eject_disk', (aksy.devices.akai.sysex_types.WORD,), None)
-          self.get_no_subfolders_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x10', 'get_no_subfolders', (), None)
-          self.get_subfolder_names_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x12', 'get_subfolder_names', (),
+          self.get_no_folders_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x10', 'get_no_folders', (), None)
+          self.get_folder_names_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x12', 'get_folder_names', (),
               (aksy.devices.akai.sysex_types.STRINGARRAY,), userref_type=aksy.devices.akai.sysex_types.S56K_USERREF)
-          self.set_curr_folder_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x13', 'set_curr_folder', (aksy.devices.akai.sysex_types.STRING,), None)
+          self.open_folder_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x13', 'open_folder', (aksy.devices.akai.sysex_types.STRING,), None)
           self.load_folder_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x15', 'load_folder', (aksy.devices.akai.sysex_types.STRING,),
               None, userref_type=aksy.devices.akai.sysex_types.S56K_USERREF)
-          self.create_subfolder_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x16', 'create_subfolder', (aksy.devices.akai.sysex_types.STRING,), None)
-          self.del_subfolder_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x17', 'del_subfolder', (aksy.devices.akai.sysex_types.STRING,), None)
-          self.rename_subfolder_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x18', 'rename_subfolder', (aksy.devices.akai.sysex_types.STRING, aksy.devices.akai.sysex_types.STRING), None)
+          self.create_folder_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x16', 'create_folder', (aksy.devices.akai.sysex_types.STRING,), None)
+          self.del_folder_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x17', 'delete_folder', (aksy.devices.akai.sysex_types.STRING,), None)
+          self.rename_folder_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x18', 'rename_folder', (aksy.devices.akai.sysex_types.STRING, aksy.devices.akai.sysex_types.STRING), None)
           self.get_no_files_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x20', 'get_no_files', (),
               (aksy.devices.akai.sysex_types.WORD,), userref_type=aksy.devices.akai.sysex_types.S56K_USERREF)
           self.get_filenames_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x22', 'get_filenames', (),
               (aksy.devices.akai.sysex_types.STRINGARRAY,), userref_type=aksy.devices.akai.sysex_types.S56K_USERREF)
-          self.rename_file_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x28', 'rename_file', (aksy.devices.akai.sysex_types.STRING, aksy.devices.akai.sysex_types.STRING), None)
-          self.delete_file_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x29', 'delete_file', (aksy.devices.akai.sysex_types.STRING,), None)
-          self.load_file_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x2A', 'load_file', (aksy.devices.akai.sysex_types.STRING,), None)
-          self.load_file_and_deps_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x2B', 'load_file_and_deps', (aksy.devices.akai.sysex_types.STRING,), None)
-          self.save_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x2C', 'save', (aksy.devices.akai.sysex_types.DWORD, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BOOL, aksy.devices.akai.sysex_types.BYTE), None)
-          self.save_all_cmd = aksy.devices.akai.sysex.Command('\x5f', '\x20\x2D', 'save_all', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BOOL), None)
+          self.rename_file_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x28', 'rename', (aksy.devices.akai.sysex_types.STRING, aksy.devices.akai.sysex_types.STRING), None)
+          self.delete_file_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x29', 'delete', (aksy.devices.akai.sysex_types.STRING,), None,
+              userref_type=aksy.devices.akai.sysex_types.S56K_USERREF)
+          self.load_file_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x2A', 'load_file', (aksy.devices.akai.sysex_types.STRING,), None)
+          self.load_file_and_deps_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x20\x2B', 'load_file_and_deps', (aksy.devices.akai.sysex_types.STRING,), None)
+          self.save_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x10\x2C', 'save',
+              (aksy.devices.akai.sysex_types.DWORD, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BOOL,
+               aksy.devices.akai.sysex_types.BOOL), None,
+              userref_type=aksy.devices.akai.sysex_types.S56K_USERREF)
+          self.save_all_cmd = aksy.devices.akai.sysex.Command('\x5e', '\x20\x2D', 'save_all', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BOOL), None,
+             userref_type=aksy.devices.akai.sysex_types.S56K_USERREF)
 
      def update_disklist(self):
           """Update the list of disks connected
@@ -85,47 +91,46 @@ class Disktools:
           """
           return self.z48.execute(self.eject_disk_cmd, (arg0, ))
 
-     def get_no_subfolders(self):
+     def get_no_folders(self):
           """Get number of sub-folders in the current folder.
 
           Returns:
-               aksy.devices.akai.sysex.PAD
                aksy.devices.akai.sysex_types.WORD
           """
-          return self.z48.execute(self.get_no_subfolders_cmd, ())
+          return self.z48.execute(self.get_no_folders_cmd, ())
 
-     def get_subfolder_names(self):
+     def get_folder_names(self):
           """Get the names of all of the sub-folders in the current folder.
 
           Returns:
                aksy.devices.akai.sysex_types.STRINGARRAY
           """
-          return self.z48.execute(self.get_subfolder_names_cmd, ())
+          return self.z48.execute(self.get_folder_names_cmd, ())
 
-     def set_curr_folder(self, arg0):
+     def open_folder(self, arg0):
           """Open Folder. This sets the current folder to be the requested one. (If <Data1> = 0, the root folder will be selected.)
           """
           return self.z48.execute(self.set_curr_folder_cmd, (arg0, ))
 
      def load_folder(self, arg0):
-          """Load Folder: the selected folder, and all its contents (including subfolders)
+          """Load Folder: the selected folder, and all its contents (including folders)
           """
           return self.z48.execute(self.load_folder_cmd, (arg0, ))
 
-     def create_subfolder(self, arg0):
-          """Create Folder: Creates a sub-folder in the currently selected folder.
+     def create_folder(self, arg0):
+          """Create Folder: Creates a folder in the currently selected folder.
           """
-          return self.z48.execute(self.create_subfolder_cmd, (arg0, ))
+          return self.z48.execute(self.create_folder_cmd, (arg0, ))
 
-     def del_subfolder(self, arg0):
-          """Delete Sub-Folder.
+     def delete_folder(self, arg0):
+          """Delete Folder.
           """
-          return self.z48.execute(self.del_subfolder_cmd, (arg0, ))
+          return self.z48.execute(self.del_folder_cmd, (arg0, ))
 
-     def rename_subfolder(self, arg0, arg1):
+     def rename_folder(self, arg0, arg1):
           """Rename Folder: <Data1> = name of folder to rename
           """
-          return self.z48.execute(self.rename_subfolder_cmd, (arg0, arg1, ))
+          return self.z48.execute(self.rename_folder_cmd, (arg0, arg1, ))
 
      def get_no_files(self):
           """Get number of files in the current folder.
@@ -143,13 +148,13 @@ class Disktools:
           """
           return self.z48.execute(self.get_filenames_cmd, ())
 
-     def rename_file(self, arg0, arg1):
-          """Rename File
+     def rename(self, arg0, arg1):
+          """Rename File or Folder
           """
           return self.z48.execute(self.rename_file_cmd, (arg0, arg1, ))
 
-     def delete_file(self, arg0):
-          """Delete File. <Data1> = name of file to delete.
+     def delete(self, arg0):
+          """Delete File or Folder <Data1> = name of file to delete.
           """
           return self.z48.execute(self.delete_file_cmd, (arg0, ))
 
@@ -164,7 +169,11 @@ class Disktools:
           return self.z48.execute(self.load_file_and_deps_cmd, (arg0, ))
 
      def save(self, arg0, arg1, arg2, arg3):
-          """Save Memory Item to Disk <Data1> = Handle of Memory Item <Data2> = Type = (1=Multi; 2=Program; 3=Sample; 4=SMF) <Data3> = (0=Skip if file exists; 1=Overwrite existing files) <Data4> = (0=Don't save children; 1=Save Children)
+          """Save Memory Item to Disk
+          <Data1> = Handle of Memory Item
+          <Data2> = Type = (1=Multi; 2=Program; 3=Sample; 4=SMF)
+          <Data3> = (0=Skip if file exists; 1=Overwrite existing files)
+          <Data4> = (0=Don't save children; 1=Save Children)
           """
           return self.z48.execute(self.save_cmd, (arg0, arg1, arg2, arg3, ))
 
