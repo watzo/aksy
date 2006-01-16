6ffb2ac5b19c5ffa6b5f85ba075fccf2552a762b
walco <none@none>
2006-01-16 21:50:14 +0000

    Name updates
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40879

--- src/aksy/gui/aksyfsview.py
+++ src/aksy/gui/aksyfsview.py
@@ -2,6 +2,7 @@ from wxPython.wx import *
 from wxPython.gizmos import wxTreeListCtrl
 
 from aksy import model
+from aksyx import USBException
 from aksy.device import Device, Devices
 import os.path, traceback, sys, copy
 
@@ -12,8 +13,8 @@ ID_MAIN_PANEL = wxNewId()
 class Frame(wxFrame):
     def __init__(self,parent,title):
         wxFrame.__init__(self,parent,wxID_ANY, title, size = ( 200,100),
-                         style=wxDEFAULT_FRAME_STYLE|wxNO_FULL_REPAINT_ON_RESIZE) 
-       
+                         style=wxDEFAULT_FRAME_STYLE|wxNO_FULL_REPAINT_ON_RESIZE)
+
         filemenu= wxMenu()
         filemenu.AppendSeparator()
         filemenu.Append(ID_EXIT,"E&xit\tCtrl+Q"," Terminate the program")
@@ -23,31 +24,28 @@ class Frame(wxFrame):
         helpmenu.Append(ID_ABOUT, "&About"," Information about this program")
 
         menuBar = wxMenuBar()
-        menuBar.Append(filemenu,"&File") 
-        menuBar.Append(self.action_menu,"&Actions") 
-        menuBar.Append(helpmenu,"&Help") 
-        self.SetMenuBar(menuBar) 
+        menuBar.Append(filemenu,"&File")
+        menuBar.Append(self.action_menu,"&Actions")
+        menuBar.Append(helpmenu,"&Help")
+        self.SetMenuBar(menuBar)
         self.SetSize((800, 600))
         self.Show(True)
         EVT_MENU(self, ID_EXIT, self.OnExit)
-        EVT_MENU(self, ID_ABOUT, self.OnAbout) 
+        EVT_MENU(self, ID_ABOUT, self.OnAbout)
 
         EVT_CLOSE(self, self.OnExit)
 
         try:
-            self.sampler = Devices.get_instance('akai', 'usb', debug=0)
-            self.sampler.init()
-        except Exception, e:
+            self.sampler = Devices.get_instance('z48', 'usb', debug=0)
+        except USBException, e:
 		    # if e[0] == "No sampler found":
             self.sampler = Devices.get_instance('mock_z48', debug=1)
-            self.sampler.init()
-            self.reportException(e)
-            return
+            # self.reportException(e)
 
     def OnAbout(self,e):
         d= wxMessageDialog(self, " Aksy, controlling your Z48 sampler\n", "About Aksy", wxOK)
-        d.ShowModal() 
-        d.Destroy() 
+        d.ShowModal()
+        d.Destroy()
 
     def OnExitMenu(self,e):
         self.on_exit()
@@ -55,7 +53,7 @@ class Frame(wxFrame):
 
     def OnExit(self,e):
         self.on_exit()
-        self.Destroy() 
+        self.Destroy()
 
     def on_exit(self):
         self.FindWindowById(ID_MAIN_PANEL).store_config()
@@ -70,8 +68,8 @@ class Frame(wxFrame):
     def reportException(self, exception):
         traceback.print_exc()
         d= wxMessageDialog( self, "%s\n" % exception[0], "An error occurred", wxOK)
-        d.ShowModal() 
-        d.Destroy() 
+        d.ShowModal()
+        d.Destroy()
 
 class AksyFSTree(wxTreeListCtrl):
     def __init__(self, parent, id, **kwargs):
@@ -86,7 +84,7 @@ class AksyFSTree(wxTreeListCtrl):
         # self.fldridx     = il.Add(wxArtProvider_GetBitmap(wxART_FOLDER,      wxART_OTHER, isz))
         #self.fldropenidx = il.Add(wxArtProvider_GetBitmap(wxART_FILE_OPEN,   wxART_OTHER, isz))
         self.fileidx     = il.Add(wxArtProvider_GetBitmap(wxART_REPORT_VIEW, wxART_OTHER, isz))
-        
+
         img_path = test_dir = os.path.join(os.path.split(__file__)[0], 'img')
         self.diskidx = il.Add(wxImage(os.path.join(img_path, 'harddisk.png'), wxBITMAP_TYPE_PNG).ConvertToBitmap())
         self.memidx = il.Add(wxImage(os.path.join(img_path, 'memory.png'), wxBITMAP_TYPE_PNG).ConvertToBitmap())
@@ -105,7 +103,7 @@ class AksyFSTree(wxTreeListCtrl):
         self.AddColumn("Size")
         self.AddColumn("Used by")
         self.AddColumn("Modified")
-        self.SetMainColumn(0) 
+        self.SetMainColumn(0)
         self.SetColumnWidth(0, 450)
 
         self.root = self.AddRoot("Z48")
@@ -141,7 +139,7 @@ class AksyFSTree(wxTreeListCtrl):
 
         """Appends an item to the tree. default is root
         """
-        if parent is None:  
+        if parent is None:
             parent = self.root
 
         print "AppendAksyItem: Child name: %s has children: %s" % (item.get_name(), item.has_children())
@@ -216,9 +214,10 @@ class AksyFSTree(wxTreeListCtrl):
             evt.Skip()
 
     def OnItemExpanding(self, evt):
+        print repr(evt)
         id = evt.GetItem()
         item = self.GetPyData(id)
-
+        if not item: return
         print "OnItemExpanding %s %s" % (id, repr(item.get_name()))
         for item in item.get_children():
             if item.get_handle() not in self._item_to_id:
@@ -261,10 +260,10 @@ class TreePanel(wxPanel):
             model.File.actions.append(action)
             self.register_menu_action(action)
 
-        EVT_MENU(parent, wxID_CUT, self.OnCut)   
-        EVT_MENU(parent, wxID_COPY, self.OnCopy)   
-        EVT_MENU(parent, wxID_PASTE, self.OnPaste)   
-        EVT_CLOSE(self, self.store_config)   
+        EVT_MENU(parent, wxID_CUT, self.OnCut)
+        EVT_MENU(parent, wxID_COPY, self.OnCopy)
+        EVT_MENU(parent, wxID_PASTE, self.OnPaste)
+        EVT_CLOSE(self, self.store_config)
 
         EVT_TREE_BEGIN_LABEL_EDIT(self, self.tree.GetId(), self.CheckRenameAction)
         EVT_TREE_END_LABEL_EDIT(self, self.tree.GetId(), self.RenameAction)
@@ -287,9 +286,9 @@ class TreePanel(wxPanel):
         self.config.Flush()
 
     """Copy and paste operations.
-    2 types: 
+    2 types:
         -copy/paste aksy items in the tree
-        -copy/paste filenames between aksy 
+        -copy/paste filenames between aksy
     """
     def OnCopy(self, evt):
         # this is always a node to be copied.
@@ -313,11 +312,11 @@ class TreePanel(wxPanel):
     def prepare_edit_action(self, item, action):
         self._action_item = item
         self._action = action
-    
+
     def complete_edit_action(self, dest_id, dest_item):
         if self._action is None or self._action_item is None:
             return False
-        
+
         new_item = self._action_item.copy(dest_item.path)
         self.tree.AppendAksyItem(dest_id, new_item)
 
@@ -332,7 +331,7 @@ class TreePanel(wxPanel):
         self.prepare_edit_action(item, 'cut')
         # XXX: check if this removes the pyData as well
         self.tree.Delete(id)
-        
+
     def OnPaste(self, evt):
         dest_id = self.tree.GetSelection()
         dest_item = self.tree.GetPyData(dest_id)
@@ -379,14 +378,14 @@ class TreePanel(wxPanel):
             id = action.id
         self.actions[id] = action
         EVT_MENU(self, id, self.ExecuteAction)
-        EVT_MENU(self.GetParent(), id, self.ExecuteAction)   
+        EVT_MENU(self.GetParent(), id, self.ExecuteAction)
 
     def RenameAction(self, evt):
         new_name = evt.GetLabel()
         # TODO: implement decent checks
         if len(new_name) == 0:
             evt.Veto()
-        
+
         id = self.tree.GetSelection()
         item = self.tree.GetPyData(id)
         item.rename(new_name)
@@ -415,7 +414,7 @@ class TreePanel(wxPanel):
             args = ()
         else:
             args = action.prolog(item)
-                
+
         if args is None:
             return
 
@@ -435,7 +434,7 @@ class TreePanel(wxPanel):
         self.recordedActions.append((action, args,))
 
     def select_directory(self, item):
-        dir_dialog = wxDirDialog(self, "Choose a destination for %s" %item.get_name(), 
+        dir_dialog = wxDirDialog(self, "Choose a destination for %s" %item.get_name(),
             style=wxDD_DEFAULT_STYLE|wxDD_NEW_DIR_BUTTON)
         dir_dialog.SetPath(self.lastdir)
         if dir_dialog.ShowModal() == wxID_OK:
@@ -446,21 +445,21 @@ class TreePanel(wxPanel):
 
         dir_dialog.Destroy()
         return retval
-        
+
     def select_file(self, item):
         # TODO: cache this dialog as it is heavy...
-        filedialog = wxFileDialog(self, "Choose a destination for %s" %item.get_name(), 
+        filedialog = wxFileDialog(self, "Choose a destination for %s" %item.get_name(),
             style=wxDD_DEFAULT_STYLE)
         filedialog.SetDirectory(self.lastdir)
         filedialog.SetFilename(item.get_name())
         if filedialog.ShowModal() == wxID_OK:
-            path = filedialog.GetPath() 
+            path = filedialog.GetPath()
         else:
             path = None
 
         filedialog.Destroy()
         return (path)
-        
+
     def contextMenu(self, e):
         # TODO: multi-select
         # items = self.tree.GetSelections()
@@ -505,17 +504,17 @@ class ActionMenu(wxMenu):
             self.Remove(item.GetId())
         for index, action in enumerate(actions):
             self.Append(action.id, action.display_name, action.display_name)
-         
+
 class AksyFileDropTarget(wxFileDropTarget):
     def __init__(self, window):
         wxFileDropTarget.__init__(self)
         self.tree = window
-                                                                                                                                                            
+
     def OnDropFiles(self, x, y, filenames):
         id, flag1, flag2 = self.tree.HitTest((x,y))
         print repr(id)
         item = self.tree.GetPyData(id)
-        if item is None or not hasattr(item, "append_child"): 
+        if item is None or not hasattr(item, "append_child"):
             return
 
         sys.stderr.writelines("\n%d file(s) dropped on %s:\n" % (len(filenames), item.get_name()))
