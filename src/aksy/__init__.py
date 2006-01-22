d335848aebd5328be281182e6342b999098e3fbe
walco <none@none>
2006-01-22 22:18:28 +0000

    Small logging enhancement
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%40891

--- src/aksy/__init__.py
+++ src/aksy/__init__.py
@@ -4,6 +4,6 @@ import logging, logging.config, logging.handlers,  time
 logger = logging.getLogger('aksy')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler('aksy_%s.log' % time.strftime("%Y%m%d"))
-formatter = logging.Formatter(fmt="%(module)s %(lineno)d %(asctime)s %(levelname)s %(message)s")
+formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s %(module)s:%(lineno)d %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
