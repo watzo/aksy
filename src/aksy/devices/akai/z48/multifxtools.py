e92c88b149de01ed96807ab4955e552a2aecc2b1
walco <none@none>
2006-09-19 20:35:43 +0000

    * Added missing commands for multifxtools
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%401027

--- src/aksy/devices/akai/z48/multifxtools.py
+++ src/aksy/devices/akai/z48/multifxtools.py
@@ -7,35 +7,56 @@ Multi FX
 __author__ =  'Walco van Loon'
 __version__=  '$Rev$'
 
-import aksy.devices.akai.sysex,aksy.devices.akai.sysex_types
+from aksy.devices.akai.sysex import Command
+
+import aksy.devices.akai.sysex_types
 
 class Multifxtools:
     def __init__(self, z48):
         self.z48 = z48
-        self.is_fxcard_installed_cmd = aksy.devices.akai.sysex.Command('_', '\x24\x01', 'is_fxcard_installed', (), None)
-        self.get_no_channels_cmd = aksy.devices.akai.sysex.Command('_', '\x24\x10', 'get_no_channels', (), None)
-        self.get_max_modules_cmd = aksy.devices.akai.sysex.Command('_', '\x24\x11', 'get_max_modules', (aksy.devices.akai.sysex_types.BYTE,), None)
-        self.get_no_cmd = aksy.devices.akai.sysex.Command('_', '\x24\x20', 'get_no', (), None)
-        self.get_name_cmd = aksy.devices.akai.sysex.Command('_', '\x24\x21', 'get_name', (aksy.devices.akai.sysex_types.WORD,), None)
-        self.get_id_cmd = aksy.devices.akai.sysex.Command('_', '\x24\x22', 'get_id', (aksy.devices.akai.sysex_types.WORD,), None)
-        self.get_param_index_output_ctrl_cmd = aksy.devices.akai.sysex.Command('_', '\x24\x24', 'get_param_index_output_ctrl', (aksy.devices.akai.sysex_types.WORD,), None)
-        self.is_channel_muted_cmd = aksy.devices.akai.sysex.Command('_', '\x27\x20', 'is_channel_muted', (), None)
-        self.get_channel_input_cmd = aksy.devices.akai.sysex.Command('_', '\x27\x21', 'get_channel_input', (aksy.devices.akai.sysex_types.BYTE,), None)
-        self.get_channel_output_cmd = aksy.devices.akai.sysex.Command('_', '\x27\x22', 'get_channel_output', (aksy.devices.akai.sysex_types.BYTE,), None)
-        self.get_by_name_cmd = aksy.devices.akai.sysex.Command('_', '\x27\x30', 'get_by_name', (aksy.devices.akai.sysex_types.BYTE,), None)
-        self.get_by_index_cmd = aksy.devices.akai.sysex.Command('_', '\x27\x31', 'get_by_index', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
-        self.is_module_enabled_cmd = aksy.devices.akai.sysex.Command('_', '\x27\x40', 'is_module_enabled', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
-        self.get_param_value_cmd = aksy.devices.akai.sysex.Command('_', '\x27\x50', 'get_param_value', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
-        self.get_param_string_cmd = aksy.devices.akai.sysex.Command('_', '\x27\x51', 'get_param_string', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
-        self.get_param_qlinkctrl_cmd = aksy.devices.akai.sysex.Command('_', '\x27\x52', 'get_param_qlinkctrl', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
-        self.set_channel_mute_cmd = aksy.devices.akai.sysex.Command('_', '\x26\x20', 'set_channel_mute', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BOOL), None)
-        self.set_channel_input_cmd = aksy.devices.akai.sysex.Command('_', '\x26\x21', 'set_channel_input', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
-        self.set_channel_output_cmd = aksy.devices.akai.sysex.Command('_', '\x26\x22', 'set_channel_output', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
-        self.set_fx_by_id_cmd = aksy.devices.akai.sysex.Command('_', '\x26\x30', 'set_fx_by_id', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.STRING), None)
-        self.set_fx_by_name_cmd = aksy.devices.akai.sysex.Command('_', '\x26\x31', 'set_fx_by_name', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.WORD), None)
-        self.enable_module_cmd = aksy.devices.akai.sysex.Command('_', '\x26\x40', 'enable_module', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BOOL), None)
-        self.set_param_value_cmd = aksy.devices.akai.sysex.Command('_', '\x26\x50', 'set_param_value', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
-        self.set_param_qlinkctrl_cmd = aksy.devices.akai.sysex.Command('_', '\x26\x52', 'set_param_qlinkctrl', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.is_fxcard_installed_cmd = Command('_', '\x24\x01', 'is_fxcard_installed', (), None)
+        self.get_no_channels_cmd = Command('_', '\x24\x10', 'get_no_channels', (), None)
+        self.get_max_modules_cmd = Command('_', '\x24\x11', 'get_max_modules', (aksy.devices.akai.sysex_types.BYTE,), None)
+        self.get_no_cmd = Command('_', '\x24\x20', 'get_no', (), None)
+        self.get_name_cmd = Command('_', '\x24\x21', 'get_name', (aksy.devices.akai.sysex_types.WORD,), None)
+        self.get_id_cmd = Command('_', '\x24\x22', 'get_id', (aksy.devices.akai.sysex_types.WORD,), None)
+        self.get_param_index_output_ctrl_cmd = Command('_', '\x24\x24', 'get_param_index_output_ctrl', (aksy.devices.akai.sysex_types.WORD,), None)
+        self.get_number_of_parameters_cmd = Command('_', '\x24\x30', 'get_number_of_parameters', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_parameter_minimum_cmd = Command('_', '\x24\x31', 'get_parameter_minimum', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_parameter_maximum_cmd = Command('_', '\x24\x32', 'get_parameter_maximum', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_parameter_name_cmd = Command('_', '\x24\x33', 'get_parameter_name', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_parameter_units_cmd = Command('_', '\x24\x34', 'get_parameter_units', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_parameter_type_cmd = Command('_', '\x24\x35', 'get_parameter_type', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_display_template_cmd = Command('_', '\x24\x36', 'get_display_template', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_parameter_display_id_cmd = Command('_', '\x24\x38', 'get_parameter_display_id', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_number_of_parameter_groups_cmd = Command('_', '\x24\x40', 'get_number_of_parameter_groups', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_group_name_cmd = Command('_', '\x24\x41', 'get_group_name', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_group_index_of_parameter_cmd = Command('_', '\x24\x42', 'get_group_index_of_parameter', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.set_channel_mute_cmd = Command('_', '\x26\x20', 'set_channel_mute', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BOOL), None)
+        self.set_channel_input_cmd = Command('_', '\x26\x21', 'set_channel_input', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.set_channel_output_cmd = Command('_', '\x26\x22', 'set_channel_output', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.set_effect_by_name_cmd = Command('_', '\x26\x30', 'set_effect_by_name', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.STRING), None)
+        self.set_effect_by_index_cmd = Command('_', '\x26\x31', 'set_effect_by_index', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.enable_fx_module_cmd = Command('_', '\x26\x40', 'enable_fx_module', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BOOL), None)
+        self.set_parameter_value_cmd = Command('_', '\x26\x50', 'set_parameter_value', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.SDWORD), None)
+        self.map_qlink_control_cmd = Command('_', '\x26\x52', 'map_qlink_control', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.is_channel_muted_cmd = Command('_', '\x27\x20', 'is_channel_muted', (aksy.devices.akai.sysex_types.BYTE,), None)
+        self.get_channel_input_cmd = Command('_', '\x27\x21', 'get_channel_input', (aksy.devices.akai.sysex_types.BYTE,), None)
+        self.get_channel_output_cmd = Command('_', '\x27\x22', 'get_channel_output', (aksy.devices.akai.sysex_types.BYTE,), None)
+        self.get_by_name_cmd = Command('_', '\x27\x30', 'get_by_name', (aksy.devices.akai.sysex_types.BYTE,), None)
+        self.get_by_index_cmd = Command('_', '\x27\x31', 'get_by_index', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.is_module_enabled_cmd = Command('_', '\x27\x40', 'is_module_enabled', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_param_value_cmd = Command('_', '\x27\x50', 'get_param_value', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_param_string_cmd = Command('_', '\x27\x51', 'get_param_string', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.get_param_qlinkctrl_cmd = Command('_', '\x27\x52', 'get_param_qlinkctrl', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.set_channel_mute_cmd = Command('_', '\x26\x20', 'set_channel_mute', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BOOL), None)
+        self.set_channel_input_cmd = Command('_', '\x26\x21', 'set_channel_input', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.set_channel_output_cmd = Command('_', '\x26\x22', 'set_channel_output', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.set_fx_by_id_cmd = Command('_', '\x26\x30', 'set_fx_by_id', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.STRING), None)
+        self.set_fx_by_name_cmd = Command('_', '\x26\x31', 'set_fx_by_name', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.WORD), None)
+        self.enable_module_cmd = Command('_', '\x26\x40', 'enable_module', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BOOL), None)
+        self.set_param_value_cmd = Command('_', '\x26\x50', 'set_param_value', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
+        self.set_param_qlinkctrl_cmd = Command('_', '\x26\x52', 'set_param_qlinkctrl', (aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE, aksy.devices.akai.sysex_types.BYTE), None)
 
     def is_fxcard_installed(self):
         """Get FX card installed
@@ -93,13 +114,141 @@ class Multifxtools:
         """
         return self.z48.execute(self.get_param_index_output_ctrl_cmd, (arg0, ))
 
-    def is_channel_muted(self):
-        """Get Mute Status of Channel <Reply> = (0=ON, 1=MUTE) BYTE
+    def get_number_of_parameters(self, arg0, arg1):
+        """Get Number of Parameters (channel, module)
+
+        Returns:
+            BYTE
+        """
+        return self.z48.execute(self.get_number_of_parameters_cmd, (arg0, arg1, ))
+
+    def get_parameter_minimum(self, arg0, arg1, arg2):
+        """Get Parameter Minimum (channel, module, param)
+
+        Returns:
+            SDWORD
+        """
+        return self.z48.execute(self.get_parameter_minimum_cmd, (arg0, arg1, arg2, ))
+
+    def get_parameter_maximum(self, arg0, arg1, arg2):
+        """Get Parameter Maximum (channel, module, param)
+
+        Returns:
+            SDWORD
+        """
+        return self.z48.execute(self.get_parameter_maximum_cmd, (arg0, arg1, arg2, ))
+
+    def get_parameter_name(self, arg0, arg1, arg2):
+        """Get Parameter Name (channel, module, param)
+
+        Returns:
+            STRING
+        """
+        return self.z48.execute(self.get_parameter_name_cmd, (arg0, arg1, arg2, ))
+
+    def get_parameter_units(self, arg0, arg1, arg2):
+        """Get Parameter Units (channel, module, param)
+
+        Returns:
+            STRING
+        """
+        return self.z48.execute(self.get_parameter_units_cmd, (arg0, arg1, arg2, ))
+
+    def get_parameter_type(self, arg0, arg1, arg2):
+        """Get Parameter TYPE (channel, module, param)
+
+        Returns:
+            BYTE
+        """
+        return self.z48.execute(self.get_parameter_type_cmd, (arg0, arg1, arg2, ))
+
+    def get_display_template(self, arg0, arg1, arg2):
+        """Get Parameter Template (channel, module, param)
+
+        Returns:
+            STRING
+        """
+        return self.z48.execute(self.get_display_template_cmd, (arg0, arg1, arg2, ))
+
+    def get_parameter_display_id(self, arg0, arg1, arg2):
+        """Get Parameter Display ID (channel, module, param)
+
+        Returns:
+            BYTE
+        """
+        return self.z48.execute(self.get_parameter_display_id_cmd, (arg0, arg1, arg2, ))
+
+    def get_number_of_parameter_groups(self, arg0, arg1):
+        """Get Number of Parameter Groups (channel, module)
+
+        Returns:
+            BYTE
+        """
+        return self.z48.execute(self.get_number_of_parameter_groups_cmd, (arg0, arg1, ))
+
+    def get_group_name(self, arg0, arg1, arg2):
+        """Get Parameter Group name (channel, module, group index)
+
+        Returns:
+            STRING
+        """
+        return self.z48.execute(self.get_group_name_cmd, (arg0, arg1, arg2, ))
+
+    def get_group_index_of_parameter(self, arg0, arg1, arg2):
+        """Get Parameter Group Index (channel, module, param)
+
+        Returns:
+            BYTE
+        """
+        return self.z48.execute(self.get_group_index_of_parameter_cmd, (arg0, arg1, arg2, ))
+
+    def set_channel_mute(self, arg0, arg1):
+        """Set Mute (channel, mute status)
+        """
+        return self.z48.execute(self.set_channel_mute_cmd, (arg0, arg1, ))
+
+    def set_channel_input(self, arg0, arg1):
+        """Set Channel Input (channel, input)
+        """
+        return self.z48.execute(self.set_channel_input_cmd, (arg0, arg1, ))
+
+    def set_channel_output(self, arg0, arg1):
+        """Set Channel Output (channel, output)
+        """
+        return self.z48.execute(self.set_channel_output_cmd, (arg0, arg1, ))
+
+    def set_effect_by_name(self, arg0, arg1, arg2):
+        """Set Effect on specified Channel (channel, module, effect)
+        """
+        return self.z48.execute(self.set_effect_by_name_cmd, (arg0, arg1, arg2, ))
+
+    def set_effect_by_index(self, arg0, arg1, arg2):
+        """Set Effect on specified Channel (channel, module, effect)
+        """
+        return self.z48.execute(self.set_effect_by_index_cmd, (arg0, arg1, arg2, ))
+
+    def enable_fx_module(self, arg0, arg1, arg2):
+        """Enable FX Module (channel, module, enabled)
+        """
+        return self.z48.execute(self.enable_fx_module_cmd, (arg0, arg1, arg2, ))
+
+    def set_parameter_value(self, arg0, arg1, arg2, arg3):
+        """Set Parameter value (channel, module, param index, value)
+        """
+        return self.z48.execute(self.set_parameter_value_cmd, (arg0, arg1, arg2, arg3, ))
+
+    def map_qlink_control(self, arg0, arg1, arg2, arg3):
+        """Maps QLink control to a Parameter (channel, module, param index, qlink no)
+        """
+        return self.z48.execute(self.map_qlink_control_cmd, (arg0, arg1, arg2, arg3, ))
+
+    def is_channel_muted(self, arg0):
+        """Get Mute Status of Channel <Reply> = (0=ON, 1=MUTE)
 
         Returns:
             BOOL
         """
-        return self.z48.execute(self.is_channel_muted_cmd, ())
+        return self.z48.execute(self.is_channel_muted_cmd, (arg0, ))
 
     def get_channel_input(self, arg0):
         """Get Channel Input <Reply> = input
