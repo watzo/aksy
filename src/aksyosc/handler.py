a969983a0c1f90752e68a5c10e3498ad7eeac04b
walco <none@none>
2007-02-05 21:22:25 +0000

    * create response OSCMessage
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%401096

--- src/aksyosc/handler.py
+++ src/aksyosc/handler.py
@@ -1,4 +1,4 @@
-from aksyosc.osc import CallbackManager
+from aksyosc.osc import CallbackManager, decodeOSC, OSCMessage
 import logging
 
 LOG = logging.getLogger('aksyosc')
@@ -15,7 +15,8 @@ class SamplerCallbackManager(CallbackManager):
         """Given OSC data, tries to call the callback with the
         right address."""
         try:
-            CallbackManager.handle(self, data, source)
+            decoded = decodeOSC(data)
+            return self.dispatch(decoded)
         except DispatchException, e:
             LOG.error("Failed to execute command ", e)
 
@@ -26,7 +27,14 @@ class SamplerCallbackManager(CallbackManager):
         address = message[0]
         section, cmd_name = self.parse_cmd_name(address)
         # skip address, typetag
-        self.sampler.execute_by_cmd_name(section, cmd_name, message[2:])
+        result = self.sampler.execute_by_cmd_name(section, cmd_name, message[2:])
+        msg = OSCMessage()
+        if not hasattr(result, '__iter__'):
+            result = [result]
+
+        for arg in result:
+            msg.append(arg) 
+        return msg
 
     def parse_cmd_name(self, address):
         comps = address.split('/')
--- src/aksyosc/tests/test_handler.py
+++ src/aksyosc/tests/test_handler.py
@@ -3,23 +3,30 @@ from aksyosc.handler import SamplerCallbackManager
 from aksyosc.osc import OSCMessage
 
 class RecordingSampler:
-    def __init__(self):
+    def __init__(self, response=None):
         self.recorded = []
+        self.response = response
     def execute_by_cmd_name(self, s, c, args):
         self.recorded.append([s, c, args])
+        return self.response
         
 class SamplerCallbackManagerTest(TestCase):
     def setUp(self):
-        self.sampler = RecordingSampler()
+        self.sampler = RecordingSampler((1,2,'a string'))
         self.handler = SamplerCallbackManager(self.sampler)
  
     def testDispatch(self):
         message = OSCMessage()
         message.setAddress("/sample/play")
         message.append("test.wav")
-        self.handler.handle(message.getBinary())
+        response = self.handler.handle(message.getBinary())
         self.assertEquals([["sample", "play", ["test.wav"]]], self.sampler.recorded)
-        
+        expected = OSCMessage()
+        expected.append(1)
+        expected.append(2)
+        expected.append('a string')
+        self.assertEquals(str(expected), str(response))
+
     def testDispatchInvalidAddress(self):
         message = OSCMessage()
         message.setAddress("/sample/play/invalid")
