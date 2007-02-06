1aa12c6aadcb1fd3f3640363966767aa9e8de548
walco <none@none>
2007-02-06 07:30:06 +0000

    * Assure responding to sender
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%401097

--- src/aksyosc/server.py
+++ src/aksyosc/server.py
@@ -1,7 +1,15 @@
 import asyncore, socket, time
-from osc import OSCMessage
 from handler import SamplerCallbackManager
 from aksy.device import Devices
+
+class Envelope:
+    def __init__(self, address, message):
+        self.address = address
+        self.message = message
+    def get_message(self):
+        return self.message
+    def get_address(self):
+        return self.address
 
 class OSCServer(asyncore.dispatcher):
     def __init__(self, address, port, callbackManager):
@@ -12,22 +20,23 @@ class OSCServer(asyncore.dispatcher):
         self.create_socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.set_reuse_addr()
         self.bind((address, port))
-        self.response = ''
+        self.response = None 
         print '%s started at %s\n\tLocal addr: %s\n' % (
            self.__class__.__name__, time.ctime(time.time()),
            address)
 
     def writeable(self):
-        return len(self.response) > 0
+        return self.response is not None
 
     def handle_write (self):
         if (self.writeable()):
-            self.sendto(self.response, 0, self.address)
-            self.response = ''
+            self.sendto(self.response.get_message(), 0, 
+                self.response.get_address())
+            self.response = None
 
     def handle_read(self):
-        data, self.address = self.recvfrom(8192)
-        self.response = self._callbackMgr.handle(data)
+        data, address = self.recvfrom(8192)
+        self.response = Envelope(address, self._callbackMgr.handle(data))
 
     def handle_connect(self):
         pass
