381952047b11bae4a737d03b9a695bc66dcf79c5
Walco van Loon <none@none>
2007-04-24 20:40:55 +0000

    * first step to customizing the build ext command
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%401154

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 """Aksy setup module
 """
 from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext
 import platform
 
 library_dirs = []
@@ -20,28 +21,35 @@ if platform.system() == "Windows":
     extra_compile_args = ["/O2"]
     library_dirs = ["C:\Program Files\LibUSB-Win32-0.1.10.1\lib\msvc"]
 
-setup(name = "aksy",
-      version = "0.1.2",
-      author = "Walco van Loon",
-      author_email = "walco at n--tree.net",
-      package_dir={'': 'src'},
-      packages=[
-          'aksy', 'aksy.devices',
-          'aksy.devices.akai',
-          'aksy.devices.akai.mock_z48',
-          'aksy.devices.akai.z48',
-          'aksy.devices.akai.s56k' ],
-      url='http://walco.n--tree.net/projects/aksy',
+class build_akyx(build_ext):
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+
+setup(name = "aksy", 
+      version = "0.1.2", 
+      author = "Walco van Loon", 
+      author_email = "walco at n--tree.net", 
+      package_dir= {'': 'src'}, 
+      packages= [
+          'aksy', 'aksy.devices', 
+          'aksy.devices.akai', 
+          'aksy.devices.akai.mock_z48', 
+          'aksy.devices.akai.z48', 
+          'aksy.devices.akai.s56k' ], 
+      url = 'http://walco.n--tree.net/projects/aksy', 
       # scripts=['scripts/checkout.py'],
-      ext_modules=[
-          Extension("aksyx",
-              sources = [ "src/aksyx/aksyx.c", "src/aksyx/aksyxusb.c",],
-              define_macros= macros,
-              library_dirs = library_dirs,
-              include_dirs = include_dirs,
-              extra_compile_args = extra_compile_args,
-              extra_link_args = extra_link_args,
-              libraries = libraries,
-          ),
-      ]
+      ext_modules = [
+          Extension("aksyx", 
+              sources = [ "src/aksyx/aksyx.c", "src/aksyx/aksyxusb.c", ], 
+              define_macros = macros, 
+              library_dirs = library_dirs, 
+              include_dirs = include_dirs, 
+              extra_compile_args = extra_compile_args, 
+              extra_link_args = extra_link_args, 
+              libraries = libraries, 
+          ), 
+      ], 
+      cmdclass = {
+         "build_ext": build_akyx, 
+      }
 )
