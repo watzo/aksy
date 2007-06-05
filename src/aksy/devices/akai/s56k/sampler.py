377815bad3c215f83b3b22a3bbe4b4fe750eb61a
Walco van Loon <none@none>
2007-06-05 20:49:47 +0000

    * boilerplating
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%401222

--- src/aksy/devices/akai/s56k/sampler.py
+++ src/aksy/devices/akai/s56k/sampler.py
@@ -2,7 +2,7 @@ import struct
 import os.path
 import sys
 
-import sysextools, disktools
+import sysextools, disktools, programtools, sampletools, multitools, songtools
 from aksy.devices.akai.sampler import Sampler
 from aksy.devices.akai import sysex
 from aksy import model
@@ -12,13 +12,22 @@ class S56K(Sampler):
     """
     def __init__(self, debug=1):
         Sampler.__init__(self, Sampler.S56K, debug)
+        # TODO: remove duplication
 
         self.disktools = disktools.Disktools(self)
         self.sysextools = sysextools.Sysextools(self)
+        self.programtools = programtools.Programtools(self)
+        self.sampletools = sampletools.Sampletools(self)
+        self.songtools = songtools.Songtools(self)
+        self.multitools = multitools.Multitools(self)
 
         model.register_handlers({model.Disk: self.disktools,
-                        model.FileRef: self.disktools})
-
+                        model.FileRef: self.disktools,
+                        model.Program: self.programtools,
+                        model.Sample: self.sampletools,
+                        model.Multi: self.multitools, 
+                        model.Song: self.songtools})
+        
         self.sysextools.enable_msg_notification(False)
         self.sysextools.enable_item_sync(False)
         
