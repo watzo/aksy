52098ed22d8c121257cd3de9bcc1d4a3b809fd37
walco <none@none>
2007-09-28 15:42:52 +0000

    * introduce setuptools
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%401350

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python
 """Aksy setup module
 """
-from distutils.core import setup, run_setup, Extension
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+from distutils.core import Extension
 from distutils.dist import Distribution
 from distutils.command.build_ext import build_ext
 import platform, os.path
 
-# macros= [("_DEBUG", 0), ('AKSY_DEBUG', '1')]
+version = "0.3-SNAPSHOT" 
+
+# macros= [("_DEBUG", 0), ("AKSY_DEBUG", "1")]
 macros= [("AKSY_DEBUG", 0)]
 
 def customize_for_platform(ext, compiler_type):
-    ext.libraries = ['usb']
+    ext.libraries = ["usb"]
 
     # Windows 
     if platform.system() == "Windows":
@@ -19,21 +25,21 @@ def customize_for_platform(ext, compiler_type):
     if compiler_type == "msvc":
         ext.libraries = ["libusb"]
         ext.extra_compile_args = ["/O2"]
-        ext.library_dirs = [os.path.join(libusb_base_dir, 'lib', 'msvc')]
+        ext.library_dirs = [os.path.join(libusb_base_dir, "lib", "msvc")]
 
     if compiler_type == "mingw32":
         ext.libraries.append("mingw32")
-        ext.library_dirs =[os.path.join(libusb_base_dir, 'lib', 'gcc')]
+        ext.library_dirs =[os.path.join(libusb_base_dir, "lib", "gcc")]
     
     # Unix flavours
     if platform.system() == "Darwin":
-        ext.extra_link_args = ['-framework CoreFoundation IOKit']
+        ext.extra_link_args = ["-framework CoreFoundation IOKit"]
 
     if compiler_type == "unix":
         libusb_base_dir = "/usr/local"
-        ext.library_dirs = [os.path.join(libusb_base_dir, 'lib')]
+        ext.library_dirs = [os.path.join(libusb_base_dir, "lib")]
         
-    ext.include_dirs = [os.path.join(libusb_base_dir, 'include')]
+    ext.include_dirs = [os.path.join(libusb_base_dir, "include")]
 
 class build_akyx(build_ext):
     def build_extension(self, ext):
@@ -41,27 +47,33 @@ class build_akyx(build_ext):
         build_ext.build_extension(self, ext)
         
 aksy_packages = [
-          'aksy', 'aksyosc', 'aksy.devices', 
-          'aksy.devices.akai', 
-          'aksy.devices.akai.mock_z48', 
-          'aksy.devices.akai.z48', 
-          'aksy.devices.akai.s56k']
+          "aksy", "aksyosc", "aksy.devices", 
+          "aksy.devices.akai", 
+          "aksy.devices.akai.mock_z48", 
+          "aksy.devices.akai.z48", 
+          "aksy.devices.akai.s56k"]
 
-aksui_packages = ['aksui', 'aksui.UI', 'aksui.ak', 'aksui.utils', 'aksui.postmod']
+aksui_packages = ["aksui", "aksui.UI", "aksui.ak", "aksui.utils", "aksui.postmod"]
 
 all_packages = []
 all_packages.extend(aksy_packages)
 all_packages.extend(aksui_packages)
 
+def create_download_url(version):
+      return "http://walco.n--tree.net/downloads/aksy-%s" % version
+
 setup(name = "aksy", 
-      version = "0.3-SNAPSHOT", 
+      version = version,
       author = "Walco van Loon", 
       author_email = "walco at n--tree.net", 
-      package_dir = {'': 'src'}, 
+      description = "Control S5000/S6000, Z4/Z8 and MPC4000 Akai sampler models with System Exclusive over USB",
+      license = "GPL",
+      package_dir = {"": "src"}, 
       packages = all_packages, 
-      package_data = {'aksui': ['ak.py.glade']},
-      url = 'http://walco.n--tree.net/projects/aksy', 
-      scripts = ['examples/aksy-get.py', 'examples/aksy-put.py', 'src/aksyfuse/aksy-fs.py', 'src/aksui/aksy-ui.py'],
+      package_data = {"aksui": ["ak.py.glade"]},
+      url = "http://walco.n--tree.net/projects/aksy", 
+      download_url = create_download_url(version),
+      scripts = ["examples/aksy-get.py", "examples/aksy-put.py", "src/aksyfuse/aksy-fs.py", "src/aksui/aksy-ui.py"],
       ext_modules = [
           Extension("aksyx",
               sources = [ "src/aksyx/aksyx.c", "src/aksyx/aksyxusb.c",],
