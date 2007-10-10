e6634f7758c968ae539516ac291c14ff5240e0de
walco <none@none>
2007-10-10 19:10:16 +0000

    * write logs to a better location
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%401356

--- src/aksy/__init__.py
+++ src/aksy/__init__.py
@@ -1,9 +1,16 @@
 # package aksy
+import logging, logging.config, logging.handlers,  time, os.path
+
 # initialize logging
-import logging, logging.config, logging.handlers,  time
 logger = logging.getLogger('aksy')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler('aksy_%s.log' % time.strftime("%Y%m%d"))
 formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s %(module)s:%(lineno)d %(message)s")
+
+logdir = os.path.expanduser('~/.aksy/logs')
+if not os.path.exists(logdir):
+    os.makedirs(logdir)
+
+handler = logging.FileHandler(os.path.expanduser('~/.aksy/logs/aksy_%s.log' % time.strftime("%Y%m%d")))
 handler.setFormatter(formatter)
+
 logger.addHandler(handler)
