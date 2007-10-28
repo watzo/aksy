b8d02eea6d96818013ef0bf2caf783178f131e5b
walco <none@none>
2007-10-28 09:33:58 +0000

    * add aif to SAMPLE regular expression
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%401401

--- src/aksy/fileutils.py
+++ src/aksy/fileutils.py
@@ -2,7 +2,7 @@ import re
 
 RE_MULTI = re.compile("\.akm$", re.IGNORECASE)
 RE_PROGRAM = re.compile("\.(akp|pgm)$", re.IGNORECASE)
-RE_SAMPLE = re.compile("\.wav$", re.IGNORECASE)
+RE_SAMPLE = re.compile("\.(wav|aif)$", re.IGNORECASE)
 RE_SONG = re.compile("\.mid$", re.IGNORECASE)
 RE_WORD = re.compile("[\w.& _()-]+$")
 RE_FILE = re.compile("\.[a-z]{3}", re.IGNORECASE)
