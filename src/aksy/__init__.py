ed4b49170216b21c5760bbc0b2dcd1052d4883e4
walco <none@none>
2007-11-01 08:04:49 +0000

    * write a separate log file for each application
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%401424

--- src/aksy/__init__.py
+++ src/aksy/__init__.py
@@ -1,19 +1,20 @@
 # package aksy
-import logging, logging.config, logging.handlers,  time, os.path
+import logging, logging.config, logging.handlers,  time, os.path, sys
 
-from config import get_config
+from aksy.config import get_config
 
 # initialize logging
 logger = logging.getLogger('aksy')
 level = getattr(logging, get_config().get('logging', 'level'))
 logger.setLevel(level)
-formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s %(module)s:%(lineno)d %(message)s")
 
 logdir = os.path.expanduser('~/.aksy/logs')
 if not os.path.exists(logdir):
     os.makedirs(logdir)
 
-handler = logging.FileHandler(os.path.expanduser('~/.aksy/logs/aksy_%s.log' % time.strftime("%Y%m%d")))
+script_name = os.path.basename(sys.argv[0])
+handler = logging.FileHandler(os.path.expanduser('~/.aksy/logs/%s_%s.log' % (script_name, time.strftime("%Y%m%d"))))
+formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s %(name)s:%(lineno)d %(message)s")
 handler.setFormatter(formatter)
 
 logger.addHandler(handler)
