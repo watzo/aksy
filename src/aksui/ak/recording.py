5fc2206c050618eef26f7d0cb851f25528887336
Walco van Loon <none@none>
2007-11-05 07:30:35 +0000

    * workaround for the invalid ascii that's returned on recordingtools.get_name() after sampler boot
    --HG--
    extra : convert_revision : svn%3Aa6520026-a7f6-0310-968f-8dd453e897c3/aksy/trunk%401456

--- src/aksui/ak/recording.py
+++ src/aksui/ak/recording.py
@@ -1,9 +1,21 @@
 import samplerobject
 
+from aksy import fileutils
+
 class Recording(samplerobject.SamplerObject):
     def __init__(self, s):
         samplerobject.SamplerObject.__init__(self, s, None, "recordingtools")
         self.attrs = ["status", "progress", "max_rec_time", "input", "mode",
 "monitor", "rec_time", "pitch", "threshold", "trigger_src", "bit_depth",
 "prerec_time", "name", "name_seed", "autorec_mode", "autonormalize"]
+        if not fileutils.is_valid_name(self.s.recordingtools.get_name()):
+            self.set("name", "Sample %i" % (self.s.sampletools.get_no_items() + 1))
         self.update()
+
+    def set(self, attrname, attrval):
+        """ Overrides handle based lookups in base class
+        """
+        tools = self.gettools()
+        getattr(tools, "set_" + attrname)(attrval)
+
+
