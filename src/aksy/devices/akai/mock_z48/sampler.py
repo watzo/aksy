db8b461b7b062ab03ca29670b94b8a7d853c6007
watzo <walco@walkit.nl>
2022-06-14 23:00:38 +0200

    #6 Format changes

--- src/aksy/devices/akai/mock_z48/sampler.py
+++ src/aksy/devices/akai/mock_z48/sampler.py
@@ -12,7 +12,7 @@ class MockConnector(object):
     def __init__(self):
         self.sample_file = ''
         self.program_file = ''
-        
+
     def get(self, filename, destpath=None, source=AkaiSampler.MEMORY):
         log.debug("Transferring file %s to host from source %i" % (filename, source))
         try:
@@ -24,19 +24,19 @@ class MockConnector(object):
             raise model.SamplerException("Failed to get ", filename)
 
     def put(self, path, remote_name=None, destination=AkaiSampler.MEMORY):
-        log.debug("Transferring file %s to sampler at remote_name %s (%i)" 
+        log.debug("Transferring file %s to sampler at remote_name %s (%i)"
                   % (path, remote_name, destination))
-    
+
     def execute(self, command, args, request_id=0):
         log.debug("execute(%s, %s)" % (repr(command), repr(args),))
         return None
-    
+
     def close(self):
         log.debug("close()")
-        
-    
+
+
 class MockZ48(Z48):
-    def __init__(self, connector, debug=1): 
+    def __init__(self, connector, debug=1):
         # TODO: enable call to super class c'tor
         Sampler.__init__(self, connector)
         self.connector = connector
@@ -44,15 +44,15 @@ class MockZ48(Z48):
         self.setup_tools()
 
         self._patch_disktools_get_disklist()
-        
+
         self._patch_systemtools()
-        
-        self.programtools.get_handles_names = lambda : (1, 'program', 2, 'program 2')
-        self.songtools.get_handles_names = lambda : (1, 'song',)
-        self.multitools.get_handles_names = lambda : (1, 'multi', 2, 'multi 2')
-        self.sampletools.get_handles_names = lambda : (1, 'sample', 2, 'sample 2')
-        self.recordingtools.get_name = lambda : 'sample'
-        
+
+        self.programtools.get_handles_names = lambda: (1, 'program', 2, 'program 2')
+        self.songtools.get_handles_names = lambda: (1, 'song',)
+        self.multitools.get_handles_names = lambda: (1, 'multi', 2, 'multi 2')
+        self.sampletools.get_handles_names = lambda: (1, 'sample', 2, 'sample 2')
+        self.recordingtools.get_name = lambda: 'sample'
+
         self.setup_model()
 
         self._populate_fs()
@@ -60,46 +60,46 @@ class MockZ48(Z48):
         self._patch_rootdisk_getdir()
 
     def set_sample(self, sample):
-	self.connector.sample_file = sample
+        self.connector.sample_file = sample
 
     def set_program(self, program):
-	self.connector.program_file = program
+        self.connector.program_file = program
 
     def _populate_fs(self):
         mellotron_folder = model.Folder('Mellotron Samples')
         choir_folder = model.Folder('Choir')
         choir_folder.children.extend(
             (model.FileRef('Mellotron/Choir/Choir.AKM', 7102),
-            model.FileRef('Mellotron/Choir/Choir.AKP', 7707),
-            model.FileRef('Mellotron/Choir/Vox1.wav'),))
+             model.FileRef('Mellotron/Choir/Choir.AKP', 7707),
+             model.FileRef('Mellotron/Choir/Vox1.wav'),))
 
         mellotron_folder.children.extend(
             (choir_folder,
-            model.FileRef('Mellotron Samples/A Sample.AKP'),
-            model.FileRef('Mellotron Samples/Sample.wav'),))
-        first_disk = self.disks.get_children()[0] 
+             model.FileRef('Mellotron Samples/A Sample.AKP'),
+             model.FileRef('Mellotron Samples/Sample.wav'),))
+        first_disk = self.disks.get_children()[0]
         first_disk.root.children = [model.Folder('Autoload'),
-             model.Folder('Songs')]
+                                    model.Folder('Songs')]
         self.disks.get_children()[1].root.children = [mellotron_folder]
 
         memory_items = [model.Sample("Boo", 1),
-            model.Multi("Default", 2),]
+                        model.Multi("Default", 2), ]
         for i in range(0, 100):
-            memory_items.append(model.Sample("Sample%i" %i, i))
+            memory_items.append(model.Sample("Sample%i" % i, i))
         self.memory.set_children(memory_items)
-        self.memory.get_children()[0].get_modified = lambda : True
+        self.memory.get_children()[0].get_modified = lambda: True
 
     def _patch_disktools_get_disklist(self):
-        def get_disklist(): 
+        def get_disklist():
             return [(256, 1, 0, 3, True, "Samples disk"),
                     (512, 1, 0, 3, False, "Cdrom")]
-        
+
         self.disktools.get_disklist = get_disklist
 
     def _patch_systemtools(self):
-        self.systemtools.get_free_wave_mem_size = lambda : 4
-        self.systemtools.get_wave_mem_size = lambda : 16
-        
+        self.systemtools.get_free_wave_mem_size = lambda: 4
+        self.systemtools.get_wave_mem_size = lambda: 16
+
     def _patch_rootdisk_getdir(self):
         def get_subdir(obj, path):
             for child in obj.get_children():
@@ -114,6 +114,6 @@ class MockZ48(Z48):
             return folder
 
         self.disks.get_dir = get_dir
-        
+
     def close(self):
         pass
